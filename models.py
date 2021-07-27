961a6866b98ffdac5701882bd36616bcbe23f1c4
Stanlag26 <Stanlag@mail.ru>
2021-07-27 16:26:01 +0300

    27/07/2021 16:01

--- models.py
+++ models.py
@@ -25,3 +25,16 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag id: {}, name: {}>'.format(self.id, self.name)
